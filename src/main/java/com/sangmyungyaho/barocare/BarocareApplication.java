3ff445cc3f52adfd00fd8f250174ad5d6599fbc5
shlee72729799 <2tjdgns811@naver.com>
2026-08-11 03:24:56 +0900

    chore: 초기 프로젝트 환경 설정

---
+++ src/main/java/com/sangmyungyaho/barocare/BarocareApplication.java
@@ -0,0 +1,13 @@
+package com.sangmyungyaho.barocare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarocareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarocareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangmyungyaho/barocare/BarocareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangmyungyaho.barocare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarocareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
