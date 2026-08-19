5153e4ac8d550a6470ae17a30ae7a313fd1b97bc
kangho <kil0653@naver.com>
2026-08-19 13:44:18 +0900

    fix: 서버 타임존 KST 고정

--- src/main/java/com/sangmyungyaho/barocare/BarocareApplication.java
+++ src/main/java/com/sangmyungyaho/barocare/BarocareApplication.java
@@ -1,10 +1,18 @@
 package com.sangmyungyaho.barocare;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BarocareApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarocareApplication.class, args);
